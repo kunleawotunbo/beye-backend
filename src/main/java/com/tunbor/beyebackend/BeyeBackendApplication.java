ec158cf3c434d242b92f37e9f0bd3887809f2662
Olakunle Awotunbo <18039413+kunleawotunbo@users.noreply.github.com>
2022-01-06 00:11:27 +0100

    initial commit

---
+++ src/main/java/com/tunbor/beyebackend/BeyeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tunbor.beyebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tunbor/beyebackend/BeyeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunbor.beyebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
