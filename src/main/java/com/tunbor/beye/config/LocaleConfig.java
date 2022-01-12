4ff3d3ac78a52c8fcdb6fca04bf1d89c716b26d6
Olakunle Awotunbo <18039413+kunleawotunbo@users.noreply.github.com>
2022-01-12 23:31:59 +0100

    support for i18n
    internationalization

---
+++ src/main/java/com/tunbor/beye/config/LocaleConfig.java
@@ -0,0 +1,30 @@
+package com.tunbor.beye.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * @author Olakunle Awotunbo
+ */
+
+@Configuration
+public class LocaleConfig {
+
+    @Bean
+    public AcceptHeaderLocaleResolver localeResolver() {
+        final AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        resolver.setDefaultLocale(Locale.US);
+        return resolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        final ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        return source;
+    }
+}
--- src/main/java/com/tunbor/beye/controller/TestController.java
+++ src/main/java/com/tunbor/beye/controller/TestController.java
@@ -5,14 +5,21 @@ import com.tunbor.beye.entity.User;
 import com.tunbor.beye.service.UserService;
 import com.tunbor.beye.utility.AppConstants;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Locale;
 
 /**
  * @author Olakunle Awotunbo
  * @since 07/01/2022
  */
+
+@Slf4j
 @RestController
 @RequestMapping(TestController.PATH)
 @RequiredArgsConstructor
@@ -20,7 +27,13 @@ public class TestController {
 
     public static final String PATH = AppConstants.VERSION_URL + "/test";
 
+    @Value("${placeholder.greetings}")
+    private String greetings;
+
     private final UserService userService;
+
+    @Autowired
+    private ResourceBundleMessageSource source;
 
     @GetMapping("/testMe")
     public AppResponse<String> test() {
@@ -32,5 +45,13 @@ public class TestController {
         User user = new User();
         user.setFirstName(firstName);
         return userService.createTestUser(user);
+    }
+
+    @GetMapping("/message")
+    public AppResponse<String> getLocaleMessage(
+            @RequestHeader(name = "Accept-Language", required = false) final Locale locale,
+            @RequestParam(name = "username", defaultValue = "Ola", required = false) final String username) {
+        log.info("Returning greetings for locale = {}", locale);
+        return AppResponse.createSuccess(source.getMessage(greetings, new Object[]{username}, locale));
     }
 }
