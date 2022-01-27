38cac1069066bed96291501ef24e3620df80371b
Olakunle Awotunbo <18039413+kunleawotunbo@users.noreply.github.com>
2022-01-27 03:10:21 +0100

    more circleci config

--- src/test/java/com/tunbor/beye/controller/TestControllerTest.java
+++ src/test/java/com/tunbor/beye/controller/TestControllerTest.java
@@ -1,14 +1,14 @@
 package com.tunbor.beye.controller;
 
-import com.tunbor.beye.service.UserService;
+//import com.tunbor.beye.service.UserService;
 import org.junit.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.Mockito.when;
-import static org.hamcrest.Matchers.*;
+//import static org.mockito.Mockito.when;
+//import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,8 +22,8 @@ public class TestControllerTest extends AbstractControllerTest {
 
     private static final String CONTROLLER_PATH = "/test";
 
-    @MockBean
-    private UserService userService;
+//    @MockBean
+//    private UserService userService;
 
     @Test
     public void testMe() throws Exception {
